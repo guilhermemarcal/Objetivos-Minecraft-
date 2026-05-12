63d3711ddaa8fff13938201ec4375507adc52d74
guilhermemarcal <guilherme.marcal.barbosa@escola.pr.gov.br>
2026-05-12 09:16:52 -0300

    12/05/2026

--- main.js
+++ main.js
@@ -38,3 +38,58 @@ horas %= 24;
     <p class="contador-digito-texto">seg</p>
   </div>
 </div>
+comecaCronometro();
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+if (tempoFinal > 0) {
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+} else {
+  return [0, 0, 0, 0];
+}
+function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[0]);
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+    for (let i = 0; i < contadores.length; i++) {
+  document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+  document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+  document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+  document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+}
+  }
+}
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  if (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0, 0, 0, 0];
+  }
+}
