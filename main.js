c69b7c0a3df030599f81fe10193d2190016f0475
guilhermemarcal <guilherme.marcal.barbosa@escola.pr.gov.br>
2026-05-12 09:05:54 -0300

    12/05/2026

--- main.js
+++ main.js
@@ -25,4 +25,16 @@ horas %= 24;
     <p class="contador-digito-numero">7</p>
     <p class="contador-digito-texto">dias</p>
   </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">horas</p>
   </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">min</p>
+  </div>
+  <div class="contador-digito">
+    <p class="contador-digito-numero">7</p>
+    <p class="contador-digito-texto">seg</p>
+  </div>
+</div>
